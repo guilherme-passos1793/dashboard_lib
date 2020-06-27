941967b0e1038f50ec8f58cda3659e65d06370c3
guilherme passos <guilherme.passos@usp.br>
2020-06-27 19:34:48 -0300

    initial commit

---
+++ dashboarding/app_page.py
@@ -0,0 +1,42 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+from .app_initialization import Application
+
+
+class Page():
+    def __init__(self, link, layout, parent):
+        """
+
+        :param link: link to the page on the app
+        :param layout: base layout on the page
+        :param parent: application from app_initialization module
+        """
+        self.layout = layout
+        # self.func_def_callbacks=func_def_callbacks
+        self.app = parent
+        self.link = link
+
+    def add_callback(self, func, outputs, inputs, states=[]):
+        """
+        adds callback to parent
+        :param func: callback function
+        :param outputs: tuple or list of tuples in the form of (id, parameter to change)
+        :param inputs: list of tuples in the form of (id, parameter to change)
+        :param states: list of tuples in the form of (id, parameter to change)
+        :return:
+        """
+        if type(outputs) == list:
+            @self.app.app.callback([dash.dependencies.Output(*out) for out in outputs],
+                               [dash.dependencies.Input(*inp) for inp in inputs],
+                               [dash.dependencies.State(*state) for state in states])
+            def function(*args):
+                return func(*args)
+        else:
+            @self.app.app.callback(dash.dependencies.Output(*outputs),
+                               [dash.dependencies.Input(*inp) for inp in inputs],
+                               [dash.dependencies.State(*state) for state in states])
+            def function(*args):
+                return func(*args)
+
---
+++ main.py
@@ -0,0 +1,15 @@
+import dashboarding.app_initialization as ai
+import dashboarding.app_page as ap
+from threading import Thread
+import dash_html_components as html
+import dash_core_components as dcc
+import page_1
+base_layout = html.Div([dcc.Location(id='url', refresh=False), html.Div(id='main_div'),
+ html.Button('teste', id='main_button')])
+app = ai.Application(basic_layout=base_layout)
+page1 = page_1.Page1(app)
+app.add_page(page1)
+
+
+
+app.start_and_open()
