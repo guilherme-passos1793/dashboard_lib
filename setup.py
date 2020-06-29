4aa99cc21a7454ef7c2e535a9d682bee6a321ff5
guilherme passos <guilherme.passos@usp.br>
2020-06-29 03:35:49 -0300

    initial commit

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] # Examples: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='dashboard_lib',
@@ -9,5 +16,5 @@ setup(
     author_email='guilherme.passos1793@gmail.com',
     license='unlicense',
     packages=['dashboard_lib'],
-    zip_safe=False
+    zip_safe=False, install_requires=install_requires
 )
