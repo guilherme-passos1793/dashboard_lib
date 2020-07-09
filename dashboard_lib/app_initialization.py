06dfd9d22b83e965cb8de81044f151b13168f22c
guilherme passos <guilherme.passos@usp.br>
2020-07-09 01:30:28 -0300

    dbc

--- dashboard_lib/app_initialization.py
+++ dashboard_lib/app_initialization.py
@@ -7,24 +7,40 @@ import webbrowser
 from threading import Thread
 import dash_bootstrap_components as dbc
 print(os.getcwd())
-
+import dash_auth
 
 class Application:
     def __init__(self, host='127.0.0.1:8050', assets_folder=os.path.join(os.getcwd(), '/assets'), auth=None,
                  title='My app',
                  basic_layout=html.Div([dcc.Location(id='url', refresh=False), html.Div(id='main_div')]),
                  page_div_id='main_div', url_id='url', export_file_path=os.path.join(os.getcwd(), '/export')):
-        self.app = dash.Dash(__name__, assets_folder=assets_folder, external_stylesheets=[dbc.themes.BOOTSTRAP])
+        """
+
+        :param host:
+        :param assets_folder:
+        :param auth: pairs of usernames and passwords
+        :type auth: dict
+        :param title:
+        :param basic_layout:
+        :param page_div_id:
+        :param url_id:
+        :param export_file_path:
+        """
+        self.app = dash.Dash(__name__, assets_folder=assets_folder, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
         self.app.title = title
         self.app.layout = basic_layout
         self.addr = host
         self.pages = {}
         self.page_div_id = page_div_id
         self.url_id = url_id
-
+        if auth:
+            basicauth = dash_auth.BasicAuth(
+                self.app,
+                auth
+            )
     def start(self):
-        self.app.run_server()
-        # self.app.run_server(host=self.addr.split(':')[0], port=os.getenv("PORT", int(self.addr.split(':')[1])))
+        # self.app.run_server()
+        self.app.run_server(host=self.addr.split(':')[0], port=os.getenv("PORT", int(self.addr.split(':')[1])))
 
     def open(self):
         webbrowser.open('http://' + self.addr)
